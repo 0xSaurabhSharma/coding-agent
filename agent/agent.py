71be51830ce679ab10d99ff2811b60a875ce6331
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2026-03-29 13:51:15 +0530

    Add context memory to agent

--- agent/agent.py
+++ agent/agent.py
@@ -3,13 +3,16 @@ from typing import AsyncGenerator
 from client.response import StreamEventType
 from client.llm_client import LLMClient
 from agent.events import AgentEventType, AgentEvent
+from context.manager import ContextManager
 
 class Agent:
     def __init__(self):
         self.client = LLMClient()
+        self.context_manager = ContextManager()
 
     async def run(self, message: str | None = None):
         yield AgentEvent.agent_start(message)
+        self.context_manager.add_user_message(message)
         # Add user message to context
 
         final_response: str | None = None
@@ -30,15 +33,15 @@ class Agent:
     async def _agentic_loop (self, message: str ) -> AsyncGenerator[AgentEvent, None]:
 
         # context manager & sessions but for now, lets keep it simple
-        messages = [
-            {
-                "role": "user",
-                "content": message or "test invoke, ans in three word only: "
-            }
-        ]
+        # messages = [
+        #     {
+        #         "role": "user",
+        #         "content": message or "test invoke, ans in three word only: "
+        #     }
+        # ]
         responseText = ""
 
-        async for event in self.client.chat_completion(messages, True):
+        async for event in self.client.chat_completion(self.context_manager.get_messages(), True):
             
             if event.type == StreamEventType.TEXT_DELTA:
                 if event.text_delta:
@@ -49,6 +52,8 @@ class Agent:
             elif event.type == StreamEventType.ERROR:
                 yield AgentEvent.agent_error(event.error or "Unknown error occured.")
 
+        
+        self.context_manager.add_assistant_message(responseText or None)
         if responseText:
             yield AgentEvent.text_complete(content=responseText)
 
---
+++ context/manager.py
@@ -0,0 +1,58 @@
+from typing import Any
+from dataclasses import dataclass
+from utils.text import count_token
+
+@dataclass
+class MessageItem:
+    role: str 
+    content: str 
+    token_count: int | None = None
+
+    def to_dict(self) -> dict[str, Any]:
+        result: dict[str, Any] = {
+            "role": self.role
+        }
+        if self.content:
+            result["content"] = self.content
+
+        return result
+
+
+class ContextManager:
+
+    def __init__(self) -> None:
+        self._system_prompt: str | None = None
+        self._model_name = "openai/gpt-oss-20b"
+        self._messages: list[MessageItem] = []
+
+    def add_user_message(self, content: str) -> None:
+        item = MessageItem(
+            role = "user",
+            content = content,
+            token_count= count_token(content,self._model_name)
+        )
+
+        self._messages.append(item)
+    
+    def add_assistant_message(self, content: str) -> None:
+        item = MessageItem(
+            role = "assistant",
+            content = content or "",
+            token_count= count_token(content,self._model_name)
+        )
+
+        self._messages.append(item)
+
+    def get_messages(self) -> list[dict[str, Any]]:
+        messages = []
+
+        if self._system_prompt:
+            messages.append({
+                "role": "system",
+                "content": self._system_prompt
+            })
+        
+        for item in self._messages:
+            messages.append(item.to_dict())
+
+        return messages
